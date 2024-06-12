55444d50db227cb4432608ac9788aa0747c2293c
DeyvisonTav <deyvisontav@gmail.com>
2024-06-11 22:13:59 -0300

    chore: config for deploy

--- src/main.ts
+++ src/main.ts
@@ -1,19 +1,26 @@
-import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
-import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
+import { join } from 'path';
+import { NestExpressApplication } from '@nestjs/platform-express';
+import { ValidationPipe } from '@nestjs/common';
 
 async function bootstrap() {
-  const app = await NestFactory.create(AppModule);
+  const app = await NestFactory.create<NestExpressApplication>(AppModule, {
+    cors: true,
+  });
+  app.enableCors();
 
   const config = new DocumentBuilder()
     .setTitle('Student Registration API')
     .setDescription('API for student registration')
     .setVersion('1.0')
     .build();
-  const document = SwaggerModule.createDocument(app, config);
-  SwaggerModule.setup('api', app, document);
 
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('swagger-ui', app, document);
+
+  app.useStaticAssets(join(__dirname, '..', 'node_modules', 'swagger-ui-dist'));
   app.useGlobalPipes(new ValidationPipe());
   await app.listen(3333);
 }
